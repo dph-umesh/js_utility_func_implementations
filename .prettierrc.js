df43bc03a3820fc0b52d310eae2a5da84df9a3fc
Umesh Kadam <umesh.kadam@synerzip.com>
2021-04-07 18:28:50 +0530

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    tabWidth: 4, // default 2
+    useTabs: false,
+    semi: true,
+    singleQuote: true, // default false
+    jsxSingleQuote: false,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    arrowParens: 'always',
+};
